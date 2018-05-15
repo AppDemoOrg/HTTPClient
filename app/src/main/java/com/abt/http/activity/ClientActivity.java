395a9adfc3f491a8679da31ebaff8195c9e98437
huangweiqi <askviky2010@gmail.com>
2018-05-16 02:14:11 +0800

    add FamousInfo

--- app/src/main/java/com/abt/http/activity/ClientActivity.java
+++ app/src/main/java/com/abt/http/activity/ClientActivity.java
@@ -1,5 +1,7 @@
 package com.abt.http.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,5 +10,10 @@ import android.support.v7.app.AppCompatActivity;
  * @创建时间： @16/05/2018
  */
 public class ClientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
 }
