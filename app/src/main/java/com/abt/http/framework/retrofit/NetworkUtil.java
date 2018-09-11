19acb02ef5aaec879dc26281ee20a2a2c247d407
WeiQi <askviky2010@gmail.com>
2018-09-11 16:03:54 +0800

    Create NetworkUtil.java

---
+++ app/src/main/java/com/abt/http/framework/retrofit/NetworkUtil.java
@@ -0,0 +1,32 @@
+
+
+
+/**
+ * @描述： @NetworkUtil
+ * @作者： @黄卫旗
+ * @创建时间： @20/05/2018
+ */
+public class NetworkUtil {
+    /**
+     * 获取活动网路信息
+     * @param context 上下文
+     * @return NetworkInfo
+     */
+    private static NetworkInfo getActiveNetworkInfo(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo();
+    }
+
+    /**
+     * 判断网络是否可用
+     * <p>需添加权限 android.permission.ACCESS_NETWORK_STATE</p>
+     */
+    public static boolean isAvailable(Context context) {
+        NetworkInfo info = getActiveNetworkInfo(context);
+        return info != null && info.isAvailable();
+    }
+
+}
+
+
